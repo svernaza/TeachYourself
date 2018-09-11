3635b5f538d7744de3fc50f196abc7301fa98d16
Sam <vernaza1992@gmail.com>
2018-09-10 23:08:17 -0700

    First Push

---
+++ Ch 1/ch_1.cpp
@@ -0,0 +1,21 @@
+//Sam Vernaza
+//Getting back into it!
+//
+#include <iostream>
+#include <cctype>
+#include <fstream>
+
+int main ()
+{
+	std::cout << "Hello, World! I'm back, baby!" << std::endl;
+
+	//Ch 1 Exercises
+	int x = 8;
+	int y = 6;
+
+	std::cout << "Subtraction is: " << x - y << std::endl;
+	std::cout << "Multiply is: " << x * y << std::endl;
+	std::cout << "End Ch. 1 " << std::endl;
+	
+	return 0;
+}
